1c640b4f8a356ef9d4d60d1249bbdf94fca2d67b
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-08-30 06:43:53 +0700

    temp fix(catppuccin): bufferline.nvim integration

--- themes/catppuccin/neovim.lua
+++ themes/catppuccin/neovim.lua
@@ -5,4 +5,16 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	
+	-- remove when this is merge: https://github.com/LazyVim/LazyVim/pull/6354
+	{
+		"catppuccin/nvim",
+		opts = function(_, opts)
+			local module = require("catppuccin.groups.integrations.bufferline")
+			if module then
+				module.get = module.get_theme
+			end
+			return opts
+		end,
+	},
 }
